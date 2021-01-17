d85edc6ef8140fb3e1d0eb8976032115e198a84f
Warit Santaputra <waritsan@gmail.com>
2021-01-17 08:47:25 +0700

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Warit Santaputra on 17/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Warit Santaputra on 17/1/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
